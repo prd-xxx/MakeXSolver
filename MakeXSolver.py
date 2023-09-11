ed430eccc31b7cb84d91cb1b00d9ec9380756d8b
prd-xxx <hoge>
2023-09-12 00:14:42 +0900

    Fractionをやめて有理数MODにしたら やや速くなった

--- MakeXSolver.py
+++ MakeXSolver.py
@@ -1,8 +1,10 @@
-from fractions import Fraction
+MOD = 2**61-1 #適当な大きい素数
+def inv(x):
+    return pow(x, MOD-2, MOD)
 class Formula:
     def __init__(self, used_bit, x, left_formula=None, right_formula=None, operand=None):
         self.used_bit = used_bit
-        self.x = x
+        self.x = x % MOD
         self.left_formula = left_formula
         self.right_formula = right_formula
         self.operand = operand
@@ -18,7 +20,7 @@ class Formula:
     def div(self, right_formula):
         assert (self.used_bit & right_formula.used_bit) == 0
         assert right_formula.x != 0
-        return Formula(self.used_bit|right_formula.used_bit, self.x / right_formula.x, self, right_formula, '/')
+        return Formula(self.used_bit|right_formula.used_bit, self.x * inv(right_formula.x), self, right_formula, '/')
     def __str__(self):
         if self.left_formula is None:
             return str(self.x)
@@ -44,7 +46,6 @@ class MakeXSolver:
         # results[s][x] には、集合sに含まれる要素i番目のA[i]をすべて使ってxを作る作り方を高々1つ格納していく
         results = [{} for _ in range(1<<self.N)]
         for i,a in enumerate(self.A):
-            a = Fraction(a)
             results[1<<i][a] = Formula(1<<i, a)
         for s in range(1,1<<self.N):
             if self.popcnt(s) <= 1: continue
@@ -62,8 +63,8 @@ class MakeXSolver:
                             results[s][x-y] = l.sub(r)
                         if x*y not in results[s] and t < u:
                             results[s][x*y] = l.prod(r)
-                        if y != 0 and x/y not in results[s]:
-                            results[s][x/y] = l.div(r)
+                        if y != 0 and x*inv(y) not in results[s]:
+                            results[s][x*inv(y)] = l.div(r)
         if self.X in results[-1]:
             print('{}={}'.format(results[-1][X], X))
         else:
