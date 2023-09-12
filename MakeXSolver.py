9c675caccc42027a9cb83b42e21a5353eb675716
prd-xxx <hoge>
2023-09-12 23:17:06 +0900

    やっぱFractionを使う形に戻す (2 3 9 9 14 が解けず)

--- MakeXSolver.py
+++ MakeXSolver.py
@@ -1,10 +1,7 @@
-MOD = 2**61-1 #適当な大きい素数
-def inv(x):
-    return pow(x, MOD-2, MOD)
 class Formula:
     def __init__(self, used_bit, x, left_formula=None, right_formula=None, operator=None):
         self.used_bit = used_bit
-        self.x = x % MOD
+        self.x = x
         self.left_formula = left_formula
         self.right_formula = right_formula
         self.operator = operator
@@ -21,7 +18,7 @@ class Formula:
     def div(self, right_formula):
         assert (self.used_bit & right_formula.used_bit) == 0
         assert right_formula.x != 0
-        return Formula(self.used_bit|right_formula.used_bit, self.x * inv(right_formula.x), self, right_formula, '/')
+        return Formula(self.used_bit|right_formula.used_bit, self.x / right_formula.x, self, right_formula, '/')
     def __str__(self):
         if self.left_formula is None:
             return str(self.x)
@@ -37,6 +34,7 @@ class Formula:
                 or self.right_formula.operator == '/' else _right
         return '{}{}{}'.format(left, self.operator, right)
 
+from fractions import Fraction
 class MakeXSolver:
     def __init__(self, A, X):
         assert len(A) <= 10, 'too many elements to solve: {}'.format(len(A))
@@ -47,7 +45,8 @@ class MakeXSolver:
         # results[s][x] には、集合sに含まれる要素i番目のA[i]をすべて使ってxを作る作り方を高々1つ格納していく
         results = [{} for _ in range(1<<self.N)]
         for i,a in enumerate(self.A):
-            results[1<<i][a] = Formula(1<<i, a)
+            x = Fraction(a)
+            results[1<<i][a] = Formula(1<<i, x)
         for s in range(1,1<<self.N):
             if bin(s).count('1') <= 1: continue
             #sの部分集合tを列挙し、u=s\t との組み合わせで作れる結果をすべて results[s]に入れる
@@ -64,8 +63,8 @@ class MakeXSolver:
                             results[s][x-y] = l.sub(r)
                         if x*y not in results[s] and t < u:
                             results[s][x*y] = l.prod(r)
-                        if y != 0 and x*inv(y) not in results[s]:
-                            results[s][x*inv(y)] = l.div(r)
+                        if y != 0 and x/y not in results[s]:
+                            results[s][x/y] = l.div(r)
         if self.X in results[-1]:
             print('{}={}'.format(results[-1][X], X))
         else:
